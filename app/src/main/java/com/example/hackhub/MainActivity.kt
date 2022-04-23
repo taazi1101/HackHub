0fbda4b49c33b8752c2f5e8ff87658badfcf22f3
Tanttinen <taavi.puolivali8@gmail.com>
2022-04-23 14:01:14 +0300

    Changed primary colors, created a navigation drawer and removed the top bar.

--- app/src/main/java/com/example/hackhub/MainActivity.kt
+++ app/src/main/java/com/example/hackhub/MainActivity.kt
@@ -1,17 +1,73 @@
 package com.example.hackhub
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var toggle: ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        println("HEllO!")
-        println("HEllO!!!!!!!!!!!!!!!sdsadad!")
-        println("HEllO!")
-        println("HEllO!!!!!!!!!!!!!!!sdsadaaaaaaaaaaaad!")
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawerLayout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
 
-        //gay
+        navView.setNavigationItemSelectedListener {
+
+            when (it.itemId) {
+                R.id.nav_home -> Toast.makeText(
+                    applicationContext,
+                    "Clicked Home",
+                    Toast.LENGTH_SHORT
+                ).show()
+                R.id.nav_File_Bruteforce -> Toast.makeText(
+                    applicationContext,
+                    "Clicked File Bruteforce",
+                    Toast.LENGTH_SHORT
+                ).show()
+                R.id.nav_IP_Sweeper -> Toast.makeText(
+                    applicationContext,
+                    "Clicked IP Sweeper",
+                    Toast.LENGTH_SHORT
+                ).show()
+                R.id.nav_new2 -> Toast.makeText(
+                    applicationContext,
+                    "Clicked new2",
+                    Toast.LENGTH_SHORT
+                ).show()
+                R.id.nav_new3 -> Toast.makeText(
+                    applicationContext,
+                    "Clicked new3",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+            }
+            true
+        }
+
+        val sweepButton: Button = findViewById(R.id.sweepButton)
+
+        sweepButton.setOnClickListener {
+            val intent = Intent(this, IpSweepper::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+            return super.onOptionsItemSelected(item)
+
     }
 }
