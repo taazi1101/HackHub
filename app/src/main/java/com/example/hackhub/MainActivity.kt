6e8a29b864697de1c2568089ec33c325bb0ff025
Tanttinen <taavi.puolivali8@gmail.com>
2022-04-23 14:50:50 +0300

    Changed primary colors, created a navigation drawer and removed the top bar. +1

--- app/src/main/java/com/example/hackhub/MainActivity.kt
+++ app/src/main/java/com/example/hackhub/MainActivity.kt
@@ -8,15 +8,18 @@ import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var toggle: ActionBarDrawerToggle
+    lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val drawerLayout: DrawerLayout = findViewById(R.id.drawerLayout)
+        drawerLayout = findViewById(R.id.drawerLayout)
         val navView: NavigationView = findViewById(R.id.nav_view)
         toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
         drawerLayout.addDrawerListener(toggle)
@@ -25,31 +28,11 @@ class MainActivity : AppCompatActivity() {
         navView.setNavigationItemSelectedListener {
 
             when (it.itemId) {
-                R.id.nav_home -> Toast.makeText(
-                    applicationContext,
-                    "Clicked Home",
-                    Toast.LENGTH_SHORT
-                ).show()
-                R.id.nav_File_Bruteforce -> Toast.makeText(
-                    applicationContext,
-                    "Clicked File Bruteforce",
-                    Toast.LENGTH_SHORT
-                ).show()
-                R.id.nav_IP_Sweeper -> Toast.makeText(
-                    applicationContext,
-                    "Clicked IP Sweeper",
-                    Toast.LENGTH_SHORT
-                ).show()
-                R.id.nav_new2 -> Toast.makeText(
-                    applicationContext,
-                    "Clicked new2",
-                    Toast.LENGTH_SHORT
-                ).show()
-                R.id.nav_new3 -> Toast.makeText(
-                    applicationContext,
-                    "Clicked new3",
-                    Toast.LENGTH_SHORT
-                ).show()
+                R.id.nav_home -> Toast.makeText(applicationContext, "Clicked Home", Toast.LENGTH_SHORT).show()
+                R.id.nav_File_Bruteforce -> Toast.makeText(applicationContext, "Clicked File Bruteforce", Toast.LENGTH_SHORT).show()
+                R.id.nav_IP_Sweeper -> replaceFragment((),it.title.toString())
+                R.id.nav_new2 -> Toast.makeText(applicationContext, "Clicked new2", Toast.LENGTH_SHORT).show()
+                R.id.nav_new3 -> Toast.makeText(applicationContext, "Clicked new3", Toast.LENGTH_SHORT).show()
 
             }
             true
@@ -61,6 +44,13 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, IpSweepper::class.java)
             startActivity(intent)
         }
+    }
+    private fun replaceFragment(fragment: Fragment,title : String){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransition = fragmentManager.beginTransaction()
+        fragmentTransition.replace(R.id.frameLayout,fragment)
+        fragmentTransition.commit()
+
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
